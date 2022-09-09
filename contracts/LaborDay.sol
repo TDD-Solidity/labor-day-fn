224440698d6deb91a695cd818ebd5dd72b364fa3
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL>
2022-09-09 19:54:23 -0400

    LaborDay contract with greet fn

---
+++ contracts/LaborDay.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract LaborDay {
+    uint256 public counter = 1;
+
+    function greet() external pure returns (string memory) {
+        return "Happy Labor Day!";
+    }
+
+    function greet2(string memory name) external pure returns (string memory) {
+        if (
+            keccak256(abi.encodePacked((name))) ==
+            keccak256(abi.encodePacked(("Santa Claus")))
+        ) {
+            return "OMG I am so glad you called this function, Santa!";
+        }
+
+        return string(abi.encodePacked("Happy Labor Day ", name, "!"));
+    }
+}
---
+++ test/LaborDay.test.ts
@@ -0,0 +1,53 @@
+import { expect } from "chai";
+import { ContractFactory } from "ethers";
+import { ethers } from "hardhat";
+
+describe("LaborDay", function () {
+
+    let laborDay: any;
+    let LaborDay: any;
+
+    before(async () => {
+
+        LaborDay = await ethers.getContractFactory("LaborDay");
+
+    })
+
+    beforeEach(async () => {
+
+        laborDay = await LaborDay.deploy();
+
+    })
+
+    describe('greet function', () => {
+
+        it('says the default happy labor day greeting', async () => {
+
+            expect(await laborDay.greet()).to.equal('Happy Labor Day!')
+
+        })
+
+        it('says the labor day greeting to a named person', async () => {
+
+            expect(await laborDay.greet2('Texas Farmer')).to.equal('Happy Labor Day Texas Farmer!')
+            expect(await laborDay.greet2('Jim_!@#$^')).to.equal('Happy Labor Day Jim_!@#$^!')
+            expect(await laborDay.greet2('الْحُرُوف الْعَرَبِيَّ')).to.equal('Happy Labor Day الْحُرُوف الْعَرَبِيَّ!')
+            expect(await laborDay.greet2('Santa Claus1')).to.equal('Happy Labor Day Santa Claus1!')
+
+        })
+
+        it('says the special greeting for Santa Claus', async () => {
+
+            expect(await laborDay.greet2('Santa Claus')).to.equal('OMG I am so glad you called this function, Santa!')
+
+        })
+
+    })
+
+    it('starts counter at one', async () => {
+
+        expect(await laborDay.counter()).to.equal(1);
+
+    })
+
+})
